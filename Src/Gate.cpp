c363b3191184b04927bf81e19767fbb96f866272
JH Seo <jh11240@naver.com>
2023-07-22 12:54:36 +0900

    Update Gate.cpp
    board의 넓이가
    가로 42, 세로 21이므로
    41일때 방향을 L로 틀어야함

--- Src/Gate.cpp
+++ Src/Gate.cpp
@@ -26,13 +26,13 @@ void Gate::whenCollided(Snake* s){
         if(secondGatePos.first==0) s->SetDirection('D');
         else if (secondGatePos.first==20) s->SetDirection('U');
         else if(secondGatePos.second==0) s->SetDirection('R');
-        else if(secondGatePos.second==20) s->SetDirection('L');
+        else if(secondGatePos.second==41) s->SetDirection('L');
     }
     else if(s->GetHead()==secondGatePos){
         s->SetHead(firstGatePos);
         if(firstGatePos.first==0) s->SetDirection('D');
         else if (firstGatePos.first==20) s->SetDirection('U');
         else if(firstGatePos.second==0) s->SetDirection('R');
-        else if(firstGatePos.second==20) s->SetDirection('L');
+        else if(firstGatePos.second==41) s->SetDirection('L');
     }
 }
